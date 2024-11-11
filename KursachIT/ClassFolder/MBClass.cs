ac4e1b39b1af8d67f7af4ede242501bdd197db78
kzeron <maseikni@gmail.com>
2024-11-11 18:48:16 +0300

    textInMB

--- KursachIT/ClassFolder/MBClass.cs
+++ KursachIT/ClassFolder/MBClass.cs
@@ -8,5 +8,35 @@ namespace KursachIT.ClassFolder
 {
     internal class MBClass
     {
+        public static void ErrorMB(Exception exception)
+        {
+            MessageBox.Show(exception.Message, "Ошибка",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public static void ErrorMB(string text)
+        {
+            MessageBox.Show(text, "Ошибка",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public static void InformationMB(string text)
+        {
+            MessageBox.Show(text, "Информация",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        public static bool QuestionMB(string text)
+        {
+            return MessageBoxResult.Yes ==
+                MessageBox.Show(text, "Вопрос",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+        public static void ExitMB()
+        {
+            bool result = QuestionMB("Вы действительно хотите выйтм?");
+            if (result)
+            {
+                App.Current.Shutdown();
+            }
+        }
+
     }
 }
