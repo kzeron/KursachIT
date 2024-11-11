481909346528b22657f8dfc3e8c74317f010c31e
maseikni <maseikni@gmail.com>
2024-11-11 16:41:13 +0300

    DataAdded

---
+++ KursachIT/DataFolder/ContextClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursachIT.DataFolder
+{
+    public partial class ITAdminEntities : DbContext
+    {
+        private static ITAdminEntities context;
+        public static ITAdminEntities GetContext()
+        {
+            if (context == null)
+                context = new ITAdminEntities();
+            return context;
+        }
+    }
+}
