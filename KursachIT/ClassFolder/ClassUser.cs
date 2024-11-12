8c0a85eac9cec3e1f076a39296cad1df463fbc74
kzeron <maseikni@gmail.com>
2024-11-12 11:49:47 +0300

    buttonauth

---
+++ KursachIT/ClassFolder/ClassUser.cs
@@ -0,0 +1,27 @@
+﻿using KursachIT.DataFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursachIT.ClassFolder
+{
+    internal class ClassUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+        public Role Role { get; set; }
+    }
+    public enum Role
+    {
+        Admin,
+        User, 
+        Technician
+    }
+
+}
--- KursachIT/PageFolder/AuthPage.xaml.cs
+++ KursachIT/PageFolder/AuthPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using KursachIT.ClassFolder;
+using KursachIT.DataFolder;
+using KursachIT.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +30,49 @@ namespace KursachIT.PageFolder
 
         private void AuthButton_Click(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(LoginTb.Text))
+            {
+                MBClass.ErrorMB("Введите логин");
+                LoginTb.Focus();
+            }
+            else if (string.IsNullOrWhiteSpace(PasswordPb.Password))
+            {
+                MBClass.ErrorMB("Введите пароль");
+            }
+            else 
+            {
+                try
+                {
+                    var user = ITAdminEntities.GetContext().User
+                        .FirstOrDefault(u => u.Login == LoginTb.Text);
+                    if (user == null)
+                    {
+                        MBClass.ErrorMB("Введен неправильный логин");
+                        LoginTb.Focus();
+                    }
+                    else if (user.Password != PasswordPb.Password)
+                    {
+                        MBClass.ErrorMB("Введен неправильный пароль");
+                    }
+                    else
+                    {
+                        switch (user.IdRole)
+                        {
+                            case 1:
+                                NavigationService.Navigate(new AdminFolder.RequestList());
+                                break;
+                            case 2:
+                                MBClass.InformationMB("Помощник");
+                                break;
 
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MBClass.ErrorMB(ex);
+                }
+            }
         }
     }
 }
