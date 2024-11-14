f8aea130cca203f29934eef1ed1573a5d83226fb
kzeron <maseikni@gmail.com>
2024-11-14 16:00:47 +0300

    partdata

---
+++ KursachIT/ClassFolder/ClassDevice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursachIT.ClassFolder
+{
+    internal class ClassDevice
+    {
+        public int IdDevice {  get; set; }
+        public string DeviceName { get; set; }
+        public string DeviceType { get; set; }
+        public int DeviceTypeId { get; set; }
+        public string SerialNumber {  get; set; }
+
+    }
+}
--- KursachIT/DataFolder/DBModel.Designer.cs
+++ KursachIT/DataFolder/DBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\masei\source\repos\KursachIT\KursachIT\DataFolder\DBModel.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\masei\Source\Repos\kzeron\KursachIT\KursachIT\DataFolder\DBModel.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
