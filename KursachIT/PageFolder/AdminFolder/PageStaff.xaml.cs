896705f79ad6e11bf68d15f18debe4d736d247d4
maseikni <maseikni@gmail.com>
2024-11-25 20:40:12 +0300

    AddEmploeyPage

--- KursachIT/PageFolder/AdminFolder/PageStaff.xaml.cs
+++ KursachIT/PageFolder/AdminFolder/PageStaff.xaml.cs
@@ -23,51 +23,51 @@ namespace KursachIT.PageFolder.AdminFolder
     /// </summary>
     public partial class PageStaff : Page
     {
-        private ObservableCollection<ClassUser> _users;
-        public PageStaff()
-        { 
-            InitializeComponent();
-            _users = new ObservableCollection<ClassUser>();
-            LoadData();
-            StaffDgList.ItemsSource = _users;
-        }
-        private void LoadData()
-        {
-            using(var context = new ITAdminEntities())
-            {
-                var staffData = (from Employers in context.Employers
-                                 join User in context.User on Employers.IdUser equals User.IdLogin
-                                 join Role in context.Role on User.IdRole equals Role.IdRole into UserGroup
-                                 from Role in UserGroup.DefaultIfEmpty()
-                                 join Office in context.Office on Employers.IdOffice equals Office.IdOffice into OfficeGroup
-                                 from Office in OfficeGroup.DefaultIfEmpty()
-                                 select new
-                                 {
-                                     User.IdLogin,
-                                     Role.NameRole,
-                                     Employers.Name,
-                                     Employers.Lastname,
-                                     Employers.Patronymic,
-                                     Employers.Office,
-                                     Employers.numberOffice,
-                                     Employers.email,
-                                     Employers.numberPhone
-                                 }).OrderBy(u => u.IdLogin)
-                                 .ToList();
-                _users.Clear();
-                foreach(var user in staffData)
-                {
-                    _users.Add(new ClassUser
-                    {
-                        IdUser = user.IdLogin,
-                        Name = user.Name,
-                        LastName = user.Lastname,
-                        Patronymic = user.Patronymic,
-                        NameOffice = user.numberOffice,
-                    });
-                }
+            private ObservableCollection<ClassUser> _users;
+            public PageStaff()
+            { 
+                InitializeComponent();
+                _users = new ObservableCollection<ClassUser>();
+                LoadData();
                 StaffDgList.ItemsSource = _users;
             }
-        }
+            private void LoadData()
+            {
+                using(var context = new ITAdminEntities())
+                {
+                    var staffData = (from Employers in context.Employers
+                                     join User in context.User on Employers.IdUser equals User.IdLogin
+                                     join Role in context.Role on User.IdRole equals Role.IdRole into UserGroup
+                                     from Role in UserGroup.DefaultIfEmpty()
+                                     join Office in context.Office on Employers.IdOffice equals Office.IdOffice into OfficeGroup
+                                     from Office in OfficeGroup.DefaultIfEmpty()
+                                     select new
+                                     {
+                                         User.IdLogin,
+                                         Role.NameRole,
+                                         Employers.Name,
+                                         Employers.Lastname,
+                                         Employers.Patronymic,
+                                         Employers.Office,
+                                         Employers.numberOffice,
+                                         Employers.email,
+                                         Employers.numberPhone
+                                     }).OrderBy(u => u.IdLogin)
+                                     .ToList();
+                    _users.Clear();
+                    foreach(var user in staffData)
+                    {
+                        _users.Add(new ClassUser
+                        {
+                            IdUser = user.IdLogin,
+                            Name = user.Name,
+                            LastName = user.Lastname,
+                            Patronymic = user.Patronymic,
+                            NameOffice = user.numberOffice,
+                        });
+                    }
+                    StaffDgList.ItemsSource = _users;
+                }
+            }
     }
 }
